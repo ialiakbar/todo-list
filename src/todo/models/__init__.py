13baf668f01b880d626369bf83d38bfd28101870
Aliakbar Golestani <golestani.aliakbar.pv@gmail.com>
2025-11-09 13:05:00 +0330

    feat: initialize Alembic configuration for database migrations

--- src/todo/models/__init__.py
+++ src/todo/models/__init__.py
@@ -3,7 +3,10 @@ import pydantic
 from .task import Task
 from .project import Project
 
+from .project_orm import ProjectORM
+from .task_orm import TaskORM
+
 pydantic.dataclasses.rebuild_dataclass(Project)
 pydantic.dataclasses.rebuild_dataclass(Task)
 
-__all__ = ["Task", "Project"]
+__all__ = ["Task", "Project", "ProjectORM", "TaskORM"]
